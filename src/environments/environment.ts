5ce14d6a2534304f798f95c26df3567bbc3cfc10
irfanabdnst <irfan.abdillah@gmail.com>
2019-06-10 22:54:29 +0700

    Init DB

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,12 @@
 export const environment = {
   production: false,
   firebase: {
-
+    apiKey: 'AIzaSyBBLNRK6pJ5VljkzAFYNGQuRiQAS2BtQeo',
+    authDomain: 'fir-course-5ca52.firebaseapp.com',
+    databaseURL: 'https://fir-course-5ca52.firebaseio.com',
+    projectId: 'fir-course-5ca52',
+    storageBucket: 'fir-course-5ca52.appspot.com',
+    messagingSenderId: '27950687905',
+    appId: '1:27950687905:web:006bb8df9eaa5453'
   }
 };
