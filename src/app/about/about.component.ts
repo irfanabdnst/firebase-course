44c128d3a7b9bc2900c11d139090c795b72e0698
irfanabdnst <irfan.abdillah@gmail.com>
2019-06-10 23:36:35 +0700

    Querying Firestore Documents using the Firebase SDK

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,25 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import 'firebase/firestore';
+
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase/app';
+
+var config = {
+  apiKey: 'AIzaSyBBLNRK6pJ5VljkzAFYNGQuRiQAS2BtQeo',
+  authDomain: 'fir-course-5ca52.firebaseapp.com',
+  databaseURL: 'https://fir-course-5ca52.firebaseio.com',
+  projectId: 'fir-course-5ca52',
+  storageBucket: 'fir-course-5ca52.appspot.com',
+  messagingSenderId: '27950687905',
+  appId: '1:27950687905:web:006bb8df9eaa5453'
+};
+
+firebase.initializeApp(config);
+
+const db = firebase.firestore();
+
+// const settings = { timestampsInSnapshots: true };
+
+// db.settings(settings);
 
 @Component({
   selector: 'about',
@@ -6,10 +27,15 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    db.doc('courses/PpCHCR8ZKAOq8s0emq3j')
+      .get()
+      .then(snap => {
+        const data: any = snap.data();
+        data.id = snap.id;
+        console.log(data);
+      });
   }
-
 }
