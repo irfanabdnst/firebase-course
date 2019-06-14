c1a5190160a09e7505933170c66a2e211a38110b
irfanabdnst <irfan.abdillah@gmail.com>
2019-06-14 20:56:56 +0700

    Auth state for navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  isLoggedIn$: Observable<boolean>;
+  isLoggedOut$: Observable<boolean>;
 
+  constructor(private afAuth: AngularFireAuth, private router: Router) {}
 
-    logout() {
+  ngOnInit() {
+    this.isLoggedIn$ = this.afAuth.authState.pipe(map(user => !!user));
+    this.isLoggedOut$ = this.isLoggedIn$.pipe(map(loggedIn => !loggedIn));
+  }
 
-    }
-
+  logout() {
+    this.afAuth.auth.signOut().then(() => this.router.navigateByUrl('login'));
+  }
 }
